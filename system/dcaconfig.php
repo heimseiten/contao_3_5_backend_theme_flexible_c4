dabba3e17c9ea4e63ded938a0271fd98e92f6885
Niels <info@heimseiten.de>
2016-07-06 14:45:44 +0200

    remove first /

---
+++ system/dcaconfig.php
@@ -0,0 +1,173 @@
+<?php
+
+// Put your custom configuration here
+
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_article']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_content']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_content']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_content']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_content']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_content']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_content']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+
+$GLOBALS['TL_DCA']['tl_news_archive']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_news_archive']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_news_archive']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_news_archive']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_news_archive']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+$GLOBALS['TL_DCA']['tl_news']['list']['operations']['feature']['icon'] = 'system/themes/icons/featured.svg'; 
+
+
+$GLOBALS['TL_DCA']['tl_calendar']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_calendar_events']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg';
+
+
+$GLOBALS['TL_DCA']['tl_faq_category']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_faq_category']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_faq_category']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_faq_category']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_faq_category']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_faq']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_faq']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_faq']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_faq']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_faq']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_faq']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg';
+
+
+$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['recipients']['icon'] = 'system/themes/icons/mgroup.svg'; 
+
+$GLOBALS['TL_DCA']['tl_newsletter']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg';
+
+$GLOBALS['TL_DCA']['tl_newsletter_recipients']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_recipients']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_recipients']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_recipients']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_recipients']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_newsletter_recipients']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg';
+
+
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['copyChilds']['icon'] = 'system/themes/icons/copychilds.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+$GLOBALS['TL_DCA']['tl_form']['list']['operations']['articles']['icon'] = 'system/themes/icons/article.svg'; 
+
+$GLOBALS['TL_DCA']['tl_form_field']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_form_field']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_form_field']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_form_field']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_form_field']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_form_field']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg';  
+
+
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['css']['icon'] = 'system/themes/icons/css.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['modules']['icon'] = 'system/themes/icons/modules.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['layout']['icon'] = 'system/themes/icons/layout.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['imageSizes']['icon'] = 'system/themes/icons/sizes.svg'; 
+$GLOBALS['TL_DCA']['tl_theme']['list']['operations']['exportTheme']['icon'] = 'system/themes/icons/theme_export.svg'; 
+
+
+$GLOBALS['TL_DCA']['tl_module']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_module']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_module']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_module']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_module']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+
+$GLOBALS['TL_DCA']['tl_layout']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_layout']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_layout']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_layout']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_layout']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_image_size']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg';
+$GLOBALS['TL_DCA']['tl_image_size']['list']['operations']['editheader']['icon'] = 'system/themes/icons/header.svg';  
+$GLOBALS['TL_DCA']['tl_image_size']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_image_size_item']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size_item']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size_item']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size_item']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size_item']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_image_size_item']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['copyChilds']['icon'] = 'system/themes/icons/copychilds.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+$GLOBALS['TL_DCA']['tl_page']['list']['operations']['articles']['icon'] = 'system/themes/icons/article.svg'; 
+
+$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+$GLOBALS['TL_DCA']['tl_templates']['list']['operations']['source']['icon'] = 'system/themes/icons/editor.svg'; 
+
+$GLOBALS['TL_DCA']['tl_member']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_member']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_member']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_member']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_member']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_user']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_user']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_user']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_user']['list']['operations']['toggle']['icon'] = 'system/themes/icons/visible.svg'; 
+$GLOBALS['TL_DCA']['tl_user']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
+
+$GLOBALS['TL_DCA']['tl_files']['list']['operations']['edit']['icon'] = 'system/themes/icons/edit.svg'; 
+$GLOBALS['TL_DCA']['tl_files']['list']['operations']['copy']['icon'] = 'system/themes/icons/copy.svg'; 
+$GLOBALS['TL_DCA']['tl_files']['list']['operations']['cut']['icon'] = 'system/themes/icons/cut.svg'; 
+$GLOBALS['TL_DCA']['tl_files']['list']['operations']['delete']['icon'] = 'system/themes/icons/delete.svg'; 
+$GLOBALS['TL_DCA']['tl_files']['list']['operations']['show']['icon'] = 'system/themes/icons/show.svg'; 
